a5a499af9257e44e9eca642950f711eceed6a4a6
joaocrespin <joaorato58@gmail.com>
2026-08-23 11:19:34 -0300

    test: adjust test to comply with new config logic

--- tests/test_config.py
+++ tests/test_config.py
@@ -10,19 +10,19 @@ def config_path(tmp_path: Path) -> Path:
 def test_missing_file_load_config(config_path: Path) -> None:
     config = Config(config_path)
     result = config.load_config()
-    assert result == {"focus_time": "25:00","break_time": "5:00",}
+    assert result == {"focus_time": 25,"break_time": 5,}
     assert config_path.exists()
 
 def test_existing_file_load_config(config_path: Path) -> None:
     config_path.parent.mkdir(parents=True)
-    config_path.write_text(json.dumps({"focus_time": "50:00", "break_time": "10:00"}))
+    config_path.write_text(json.dumps({"focus_time": 50, "break_time": 10}))
     config = Config(file_path=config_path)
     result = config.load_config()
-    assert result == {"focus_time": "50:00", "break_time": "10:00"}
+    assert result == {"focus_time": 50, "break_time": 10}
 
 def test_set_config(config_path: Path) -> None:
     config = Config(file_path=config_path)
     config.load_config()
-    config.set_config("30:00", "7:00")
+    config.set_config(30, 7)
     result = config.load_config()
-    assert result == {"focus_time": "30:00", "break_time": "7:00"}
+    assert result == {"focus_time": 30, "break_time": 7}
